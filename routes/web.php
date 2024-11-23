50b6bc2507d7ac8ca71545d2fe9048470ff9394b
Trólka Kacper <97135299+Tkapcer@users.noreply.github.com>
2024-11-23 13:36:49 +0100

    pusty szablon klas

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,12 @@ Route::post('/rejestracja', [ControllerRejestracja::class, 'rejestracja']);
 
 Auth::routes();
 
+//Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+//Route::post('login', [LoginController::class, 'login']);
+//Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+//
+//Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+//Route::post('register', [RegisterController::class, 'register']);
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
