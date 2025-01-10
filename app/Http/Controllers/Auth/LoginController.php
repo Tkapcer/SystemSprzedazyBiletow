669641a76ae085eeb1000c6f1f2f64f300e0aa52
Trólka Kacper <97135299+Tkapcer@users.noreply.github.com>
2025-01-10 21:52:24 +0100

    Przekierowanie admina na stronę admina

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,14 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    public function redirectTo()
+    {
+        $user = auth()->user();
+
+        if ($user->type == 'admin') {
+            return '/adminPanel';
+        }
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Auth::routes();
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/adminPanel', function () {
+    return view('adminPanel');
+});
