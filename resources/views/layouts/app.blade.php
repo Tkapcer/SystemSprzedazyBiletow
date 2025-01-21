0c061f97f779cf87c54f6239a98a6d0adba27a1e
Perlaa77 <aleksandra.ha7@gmail.com>
2025-01-21 17:55:40 +0100

    pasek dalej

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,59 +18,43 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'System Sprzedaży Biletów') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <!-- Pasek nawigacyjny -->
+        <nav class="navbar">
+            <a class="nav-link" href="/">Strona Główna</a>
+            <div class="navbar-nav">
+                <!-- Jeśli użytkownik nie jest zalogowany -->
+                @guest
+                    <a class="nav-link" href="{{ route('register') }}">Rejestracja</a>
+                    <a class="nav-link" href="{{ route('login') }}">Logowanie</a>
+                @else
+                    <!-- Dla klientów -->
+                    @auth('web')
+                        <span class="user-info">
+                            <span class="user-name">{{ Auth::user()->name }}</span>
+                            <span class="user-saldo">{{ Auth::user()->balance }} zł</span>
+                            <a class="nav-link" href="{{ route('home') }}">Konto</a>
+                            <a class="nav-link" href="{{ route('home') }}">Doładuj</a> <!-- Przekierowanie na stronę konta klienta -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                    </ul>
+                            <!-- Formularz wylogowywania -->
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-inline">
+                                @csrf
+                                <button type="submit" class="nav-link logout" style="background: none; border: none; color: inherit; padding: 5px 10px;">Wyloguj</button>
+                            </form>
+                        </span>
+                    @endauth
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        @if(!auth('web')->check() && !auth('organizer')->check() && !auth('admin'))
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    @if(auth('web')->check())
-                                        {{ auth('web')->user()->name }}
-                                    @elseif(auth('organizer')->check())
-                                        {{ auth('organizer')->user()->companyName }}
-                                    @endif
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                    <!-- Dla organizatorów -->
+                    @auth('organizer')
+                        <span class="user-info">
+                            <span class="user-name">{{ Auth::user()->company_name }}</span>
+                            <a class="nav-link" href="{{ route('panel') }}">Panel</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-inline">
+                                @csrf
+                                <button type="submit" class="nav-link logout" style="background: none; border: none; color: inherit; padding: 5px 10px;">Wyloguj</button>
+                            </form>
+                        </span>
+                    @endauth
+                @endguest
             </div>
         </nav>
 
