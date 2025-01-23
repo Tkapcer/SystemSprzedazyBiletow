7ef14001752a95edf2eeeeb1bc00272d28ba890f
Mycelion <filipmorchat@interia.pl>
2025-01-23 20:41:17 +0100

    poprawa lekka adminPanel

--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -1,9 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="row">
+    <div class="container">
+        <!-- First card -->
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">{{ __('Organizatorzy') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <table class="table table-bordered mt-3">
+                        <thead>
+                            <tr>
+                                <th>Nazwa firmy</th>
+                                <th>E-Mail</th>
+                                <th>Akcje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($organizers as $organizer)
+                                <tr>
+                                    <td scope="row">{{ $organizer->companyName }}</td>
+                                    <td scope="row">{{ $organizer->email }}</td>
+                                    <th scope="row">{{ $organizer->status }}</th>
+
+                                    @if($organizer->status == 'waiting' || $organizer->status == 'rejected')
+                                        <td scope="row">
+                                            <form action="{{ route('admin.confirm', $organizer->id) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-success">Potwierdź</button>
+                                            </form>
+                                        </td>
+                                    @endif
+
+                                    @if ($organizer->status == 'approved' || $organizer->status == 'waiting')
+                                        <td scope="row">
+                                            <form action="{{ route('admin.reject', $organizer->id) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger">Odrzuć</button>
+                                            </form>
+                                        </th>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <!-- Second card -->
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Panel administratora') }}</div>
 
@@ -13,89 +66,40 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <h5>
-                        <a class="btn btn-primary" data-bs-toggle="collapse" href="#organizersSection" role="button" aria-expanded="false" aria-controls="organizersSection">
-                            Organizatorzy (kliknij, aby rozwinąć)
-                        </a>
-                    </h5>
-                    <div class="collapse" id="organizersSection">
-                        <table class="table table-bordered mt-3">
-                            <thead>
+                    <table class="table table-bordered mt-3">
+                        <thead>
                             <tr>
-                                <th>Nazwa firmy</th>
-                                <th>E-Mail</th>
+                                <th>Tytuł wydarzenia</th>
+                                <th>Status</th>
                                 <th>Akcje</th>
                             </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($organizers as $organizer)
-                                    <tr>
-                                        <th scope="row">{{ $organizer->companyName }}</th>
-                                        <th scope="row">{{ $organizer->email }}</th>
-                                        <th scope="row">{{ $organizer->status }}</th>
-
-                                        @if($organizer->status == 'waiting' || $organizer->status == 'rejected')
-                                            <th scope="row">
-                                                <form action="{{ route('admin.confirm', $organizer->id) }}" method="POST">
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-success">Potwierdź</button>
-                                                </form>
-                                            </th>
-                                        @endif
-
-                                        @if ($organizer->status == 'approved' || $organizer->status == 'waiting')
-                                            <th scope="row">
-                                                <form action="{{ route('admin.reject', $organizer->id) }}" method="POST">
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-danger">Odrzuć</button>
-                                                </form>
-                                            </th>
-                                        @endif
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <h5>
-                        <a class="btn btn-primary" data-bs-toggle="collapse" href="#eventsSection" role="button" aria-expanded="false" aria-controls="eventsSection">
-                            Wydarzenia (kliknij, aby rozwinąć)
-                        </a>
-                    </h5>
-                    <div class="collapse" id="eventsSection">
-                        <table class="table table-bordered mt-3">
-                        <thead>
-                        <tr>
-                            <th>Tytuł wydarzenia</th>
-                            <th>Status</th>
-                            <th>Akcje</th>
-                        </tr>
-                        @foreach($events as $event)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('event.show', $event->id) }}" class="btn btn-primary" role="button">
-                                        {{ $event->name }}
-                                    </a>
-                                </td>
-                                <td>{{ $event->status }}</td>
-                                <td>
-                                    @if($event->status == 'waiting' || $event->status == 'rejected')
-                                        <form action="{{ route('admin.approveEvent', $event->id) }}" method="POST">
-                                            @csrf
-                                            <button type="submit" class="btn btn-success">Potwierdź</button>
-                                        </form>
-                                    @endif
-                                    @if($event->status == 'approved' || $event->status == 'waiting')
-                                        <form action="{{ route('admin.rejectEvent', $event->id) }}" method="POST">
-                                            @csrf
-                                            <button type="submit" class="btn btn-danger">Odrzuć</button>
-                                        </form>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
                         </thead>
+                        <tbody>
+                            @foreach($events as $event)
+                                <tr>
+                                    <td>
+                                        <a href="{{ route('event.show', $event->id) }}" class="btn btn-primary" role="button">
+                                            {{ $event->name }}
+                                        </a>
+                                    </td>
+                                    <th>{{ $event->status }}</th>
+                                    <td>
+                                        @if($event->status == 'waiting' || $event->status == 'rejected')
+                                            <form action="{{ route('admin.approveEvent', $event->id) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-success">Potwierdź</button>
+                                            </form>
+                                        @endif
+                                        @if($event->status == 'approved' || $event->status == 'waiting')
+                                            <form action="{{ route('admin.rejectEvent', $event->id) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger">Odrzuć</button>
+                                            </form>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
