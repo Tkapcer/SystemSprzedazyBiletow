09f772a13978f0439c41e7b6cdc6257d8112b47e
Perlaa77 <aleksandra.ha7@gmail.com>
2025-01-23 23:41:59 +0100

    pasek

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -5,7 +5,7 @@
 <!-- Dane do testowania -->
 @php
     $event = [
-        'name' => 'Występ Kabaretu 123',
+        'name' => 'C Występ Kabaretu 123',
         'event_date' => '2024-12-15 20:00:00',
         'location' => 'Sala Koncertowa',
         'image_path' => 'images_for_testing/o3.png'
@@ -21,7 +21,7 @@
 
 <div class="container">
     <!-- Nagłówek formularza zakupu biletów -->
-    <div class="card-header">{{ __('Wybór biletów') }}</div>
+    <h1 class="text-center mb-4">Wybór biletów</h1>
 
     <!-- Informacje o wydarzeniu -->
     <div class="mb-4">
@@ -35,23 +35,34 @@
     <form action="{{ route('ticket.store') }}" method="POST">
         @csrf
 
-        <!-- Wybór sektora -->
-        <div class="form-group">
-            <label for="sector_id">Wybierz sektor:</label>
-            <select name="sector_id" id="sector_id" class="form-control" required>
+        <!-- Tabela wyboru biletów -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Rodzaj</th>
+                    <th scope="col">Cena</th>
+                    <th scope="col">Liczba biletów</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach($sectors as $sector)
-                    <option value="{{ $sector['id'] }}">
-                        {{ $sector['name'] }} (Dostępne miejsca: {{ $sector['availableSeats'] }}, Cena: {{ $sector['price'] }} zł)
-                    </option>
-                @endforeach
-            </select>
-        </div>
+                    <tr>
+                        <!-- Nazwa sektora -->
+                        <td>{{ $sector['name'] }}</td>
 
-        <!-- Wybór liczby miejsc -->
-        <div class="form-group">
-            <label for="number_of_seats" class="form-label">Wybierz liczbę miejsc</label>
-            <input type="number" class="form-control" id="number_of_seats" name="number_of_seats" value="1" required>
-        </div>
+                        <!-- Cena sektora -->
+                        <td>{{ $sector['price'] }} zł</td>
+
+                        <!-- Wybór liczby biletów -->
+                        <td>
+                            <input type="number" class="form-control" name="sectors[{{ $sector['id'] }}][number_of_seats]" value="0" min="0" max="10" required>
+                            <input type="hidden" name="sectors[{{ $sector['id'] }}][sector_id]" value="{{ $sector['id'] }}">
+                            <input type="hidden" name="sectors[{{ $sector['id'] }}][price]" value="{{ $sector['price'] }}">
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
         <!-- Dwa przyciski do wyboru akcji -->
         <div class="mt-3">
