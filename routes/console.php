ef7aa77e6eb70ce3d94c51bbadb00b35867bd581
Trólka Kacper <97135299+Tkapcer@users.noreply.github.com>
2025-01-28 00:23:00 +0100

    Zmiana statusu wydarzenia na expired dla odbytych wydarzeń

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('UpdateEventsStatus', function () {
+//    \Log::info("Updating events status");
+    $expiredEvents = Event::where('status', 'approved')
+        ->where('event_date', '<', Carbon::now());
+    foreach ($expiredEvents->get() as $event) {
+        $event->status = 'expired';
+        $event->save();
+    }
+})->purpose('Ustawienie statusu odbytych wydarzeń')->everyMinute();
